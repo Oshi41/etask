61e9e362def4696cf3a97d8f96aee89bce9498ba
Oshi41 <sheglovaoe@gmail.com>
2024-09-08 21:28:36 +0300

    implementation

---
+++ etask.js
@@ -0,0 +1,112 @@
+/**
+ * @typedef {Promise | Generator | AsyncGenerator | any} YieldFunctionResult
+ */
+
+/**
+ * @function
+ * @generator
+ * @typedef {(this: ThisArg) => Generator | AsyncGenerator} GenFunc
+ */
+
+/**
+ * @typedef {Object} ThisArg
+ * @property {(cb: ()=>YieldFunctionResult)=>void} then - Callback executed after success execution
+ * @property {(cb: ()=>YieldFunctionResult)=>void} finally - Callback executed after all
+ * @property {(cb: (e: Error)=>YieldFunctionResult)=>void} catch - Catching errors
+ * @property {(cb: (mls: number, cb: ()=>YieldFunctionResult)=>YieldFunctionResult)=>void} after - Execute after delay
+ * if main function is still executing. Perfect place for timeout handling
+ * @property {(e: Error) => void} throw Throws an error and stop execution
+ * @property {(e: any) => void} return Returns value and stop execution
+ * @property {(mls: number) => Promise} sleep - pause execution
+ * @property {Date} stats.start - executing timestamp start
+ * @property {Date} stats.end - end of successfull execution
+ */
+
+async function promisify(fn) {
+    // generator
+    if (fn?.next) {
+        let step;
+        while (!step?.done) {
+            // possible async generator
+            step = await promisify(fn.next());
+            // possible yield new Promise()
+            step.value = await promisify(step.value);
+        }
+        return step.value;
+
+        // async
+    } else if (fn?.then) {
+        return await fn;
+    }
+
+    // sync
+    return fn;
+}
+
+/**
+ * @param arr {[]}
+ * @param params {any}
+ * @returns {Promise<void>}
+ */
+async function execute_array(arr, ...params) {
+    if (!arr?.length) return;
+
+    for (let fn of arr) {
+        try {
+            await promisify(fn(...params));
+        } catch (e) {
+            console.error('Error during callback execution:', e);
+            throw e;
+        }
+    }
+}
+
+/**
+ *
+ * @param fn {GeneratorFunction} generator func
+ * @returns {Promise<any>}
+ */
+export default function etask(fn) {
+    const cb = {then: [], catch: [], finally: []};
+    return new Promise(async (resolve, reject) => {
+        let executed = false, promise_result;
+        const this_arg = {
+            sleep: mls => new Promise(resolve => setTimeout(resolve, mls)),
+            throw: reject,
+            return: resolve,
+            then: fn => cb.then.push(fn),
+            catch: fn => cb.catch.push(fn),
+            finally: fn => cb.finally.push(fn),
+            after: (mls, fn) => {
+                this_arg.sleep(mls).then(() => {
+                    // still running
+                    if (!executed) {
+                        return fn();
+                    }
+                });
+            },
+            stats: {
+                start: new Date(),
+            },
+        };
+
+        try {
+            promise_result = await promisify(fn.call(this_arg));
+            executed = true;
+            this_arg.stats.end = new Date();
+            await execute_array(cb.then);
+        } catch (e) {
+            executed = true;
+            if (cb.catch?.length)
+                await execute_array(cb.catch, e);
+            else
+                return reject(e);
+
+        } finally {
+            // executing then finally
+            await execute_array(cb.finally);
+        }
+
+        return resolve(promise_result);
+    });
+}
--- tests.js
+++ tests.js
@@ -4,20 +4,20 @@ import {deepEqual as de, fail} from 'node:assert';
 describe('index', () => {
     it('then/finally', async () => {
         let count = 0;
-        const result = await etask(function*(){
-            this.then(()=>{
-               de(count, 1);
-               count++;
+        const result = await etask(function* () {
+            this.then(() => {
+                de(count, 1);
+                count++;
             });
-            this.then(()=>{
+            this.then(() => {
                 de(count, 2);
                 count++;
             });
-            this.finally(()=>{
+            this.finally(() => {
                 de(count, 3);
                 count++;
             });
-            this.finally(()=>{
+            this.finally(() => {
                 de(count, 4);
                 count++;
             });
@@ -28,14 +28,83 @@ describe('index', () => {
         de(count, 5);
         de(result, 1);
     });
-    it('return stop the execution', async () => {
-        const result = await etask(function* () {
-            this.then(() => {
-                this.return(5)
+    it('throw stop the execution', async () => {
+        const error = new Error('some error');
+        try {
+            await etask(function* () {
+                try {
+                    yield this.throw(error);
+                } catch (e) {
+                    yield console.log('here');
+                }
+
+                return 10;
             });
-            yield this.return(1);
+            fail('Must fail');
+        } catch (e) {
+            de(e, error);
+        }
+    });
+    it('return stop the execution', async () => {
+        let count = 0;
+        const result = await etask(function* () {
+            yield this.return(5);
+            yield count++;
             return 10;
         });
-        de(result, 1);
+        de(result, 5);
+        de(count, 0);
+    });
+    it('timeout example', async () => {
+        let count = 0;
+        try {
+            await etask(function* () {
+                this.after(10, () => {
+                    this.throw('timeout')
+                });
+                yield this.sleep(20);
+                yield count++;
+                return 10;
+            });
+            fail('must fail on timeout')
+        } catch (e) {
+            de(e, 'timeout');
+        }
+    });
+    it('timeout example, will not throw an error', async () => {
+        let count = 0;
+        const result = await etask(function* () {
+            this.after(100, () => {
+                this.throw('timeout')
+            });
+            yield this.sleep(20);
+            yield count++;
+            return 10;
+        });
+        de(count, 1);
+        de(result, 10);
+    });
+    it('catch, return special value in case of error', async () => {
+        let count = 0;
+        const result = await etask(function* () {
+            this.catch(() => this.return('error'));
+            throw new Error('some error');
+            count++;
+            return 'success';
+        });
+        de(result, 'error');
+        de(count, 0);
+    });
+    it('execution time logging', async () => {
+        const result = await etask(function* () {
+            this.finally(() => {
+                const diff = this.stats.end - this.stats.start;
+                de(diff > 20, true);
+            });
+            yield this.sleep(20);
+
+            return 7;
+        });
+        de(result, 7);
     });
 });
